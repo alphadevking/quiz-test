7f99c6b69166642643dbc05fc6089753daf5091e
alphadevking <danielose266@gmail.com>
2023-04-05 23:31:53 +0100

    bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Header from '@/site/sections/header'
 import Quiz from '@/site/sections/quiz'
 import Head from 'next/head'
-import Script from 'next/script'
 
 export default function Home() {
   return (
@@ -16,9 +15,6 @@ export default function Home() {
         <Header/>
         <Quiz/>
       </main>
-      <Script>
-        <script src="http://localhost:8097"></script>
-      </Script>
     </>
   )
 }
